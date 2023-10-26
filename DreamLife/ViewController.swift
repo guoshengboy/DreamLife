7df38f14032176387e9a04c4dd4c3dfc43a195e2
caoguosheng <caoguosheng.smg.cn>
2023-10-26 09:50:25 +0800

    init

---
+++ DreamLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DreamLife
+//
+//  Created by 曹国盛 on 2023/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
